32bfac82cab05cf11b1bb59004849b59498dad75
Alavutdin <alavutdin_atlan@mail.ru>
2024-04-02 09:37:13 +0300

    Initial commit7

---
+++ src/main/java/ru/geekbrains/Spring_SecurityHomeWork7/SpringSecurityHomeWork7Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.Spring_SecurityHomeWork7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHomeWork7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHomeWork7Application.class, args);
+	}
+
+}
